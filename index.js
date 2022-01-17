af9743836044ed53bcc3eaee042157ffb731a136
Najwaageel <Najwa.Ageel.abt.js@tuwaiq.edu.sa>
2022-01-17 15:30:21 +0300

    اثقخنع

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const users = require("./routes/users")
 const games = require("./routes/games")
 
 mongoose
-  .connect(`mongodb+srv://najwa:${process.env.MONGODB_PASSWORD1}@cluster0.hrnbt.mongodb.net/gamesDB?retryWrites=true&w=majority`)
+  .connect(`mongodb+srv://najwa:${process.env.MONGODB_PASSWORD1}@cluster0.hrnbt.mongodb.net/gamesDB?retryWrites=true&w=majority`) // ع الايكلاود
   .then(() => console.log(" Connected to MongoDB"))
   .catch(error => console.log("Error connecting to MongoDB", error))
 
@@ -26,5 +26,5 @@ app.use("/api/games", games)
 
 
 
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log("server is listening on port :" + port))
